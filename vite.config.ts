504e6d3d35c3c066f5f6c83af0b493b2add95707
nobuo-honma <spdv6sh9@gmail.com>
2026-06-05 16:45:40 +0900

    GitHub Actionsの自動デプロイ設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    basicSsl()
   ],
+  // ⭐️ 超重要：前後に必ずスラッシュ ( / ) を入れてください！
+  // 大文字・小文字もGitHubのURLと完全に一致させる必要があります。
   base: '/AI-Receipt-OCR-Pro/',
+
+  build: {
+    outDir: 'dist',
+  }
 })
